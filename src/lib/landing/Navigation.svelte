54bb8682c881876a5e7ee5099b545e18f50d50e9
Alex Dalene <aledal51469@stud.noroff.no>
2024-01-11 11:52:44 +0100

    Adds blurry-gradient background with animation

---
+++ src/lib/landing/Navigation.svelte
@@ -0,0 +1,33 @@
+<ul>
+    <li><a href="/brand-identity">Brand Identity</a></li>
+    <li><a href="/logo-design">Logo Design</a></li>
+    <li><a href="/packaging">Packaging</a></li>
+    <li><a href="/posters">Posters</a></li>
+    <li><a href="/ux-ui">UX / UI</a></li>
+</ul>
+
+<style>
+    ul {
+        display: flex;
+        flex-direction: column;
+        gap: 0.25rem;
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        text-transform: uppercase;
+        font-family: 'Trap', sans-serif;
+        font-weight: 700;
+        font-size: 1.5rem;
+        margin-top: 4rem;
+
+        & a {
+            color: #1C1C1C;
+            text-decoration: none;
+            transition: color 150ms ease-in;
+
+            &:hover {
+                color: #E49644;
+            }
+        }
+    }
+</style>
